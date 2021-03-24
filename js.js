b8b028f617c08685d711293b922678558628ea91
kipprei <mkipprei@gmail.com>
2021-03-24 14:09:07 -0700

    changed menu

--- js.js
+++ js.js
@@ -1,15 +1,25 @@
 var momdad = document.getElementById("momdadmp3");
 var jingle = document.getElementById("jinglemp3");
+var menu = document.getElementById("menu");
+var body = document.getElementsByTagName("body")[0];
 
-function PlayMomDad()
-{
+function PlayMomDad() {
     if (momdad.paused) momdad.play();
     else momdad.pause();
 }
 
-function PlayJingle()
-{
+function PlayJingle() {
     if (jingle.paused) jingle.play();
     else jingle.pause();
 }
 
+function OpenMenu() {
+    menu.style.visibility = "visible";
+    menu.style.backgroundColor = "rgba(0, 0, 0, .4)";
+    //menu.style.backdropFilter = "blur(8px)";
+}
+
+function CloseMenu() {
+    menu.style.visibility = "hidden";
+    menu.style.backgroundColor = "rgba(0,0,0,0)";
+}
