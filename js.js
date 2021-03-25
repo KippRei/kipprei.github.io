1f6baef5a9ba7a036c170e577a34f88391e92e0b
kipprei <mkipprei@gmail.com>
2021-03-24 19:28:32 -0700

    working on featured audio TODO: possibly progress bar

--- js.js
+++ js.js
@@ -2,10 +2,19 @@ var momdad = document.getElementById("momdadmp3");
 var jingle = document.getElementById("jinglemp3");
 var menu = document.getElementById("menu");
 var body = document.getElementsByTagName("body")[0];
+var mdPP = document.getElementById("mdPP"); //play/pause button for mom and dad
 
 function PlayMomDad() {
-    if (momdad.paused) momdad.play();
-    else momdad.pause();
+    if (momdad.paused) 
+    {
+        momdad.play();
+        mdPP.src = "/pauseButton.png";
+    }
+    else 
+    {
+        momdad.pause();
+        mdPP.src = "/playButton.png";
+    }
 }
 
 function PlayJingle() {
@@ -17,7 +26,6 @@ function OpenMenu() {
     document.getElementById("openButton").style.visibility="hidden";
     menu.style.visibility = "visible";
     menu.style.backgroundColor = "rgba(0, 0, 0, .6)";
-    //menu.style.backdropFilter = "blur(8px)";
 }
 
 function CloseMenu() {
