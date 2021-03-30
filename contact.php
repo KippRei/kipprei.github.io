8bf084040e9cbc5139f194fda48288251b332507
kipprei <mkipprei@gmail.com>
2021-03-29 19:49:47 -0700

    working on mail

--- contact.php
+++ contact.php
@@ -16,7 +16,6 @@
             $msg = $_REQUEST["message"];
             echo $msg;
             $msg = wordwrap($msg, 70);
-            $headers = "From: kippsta0@gmail.com";
             mail("kippsta0@gmail.com", "Message", $msg, $headers);
         }
       ?>
